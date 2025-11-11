72ad10b282578f7330c4d54c5e49cddd80e90e8b
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-11 13:08:48 +0000

    Revert "Remove docs"
    This reverts commit bbc3a8d687eda879cb0d147add3a62ce65ae4121.

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    extends: ["docus"],
+
+    css: ["~/assets/css/main.css"],
+
+    site: {
+        name: "Elk",
+        url: "https://docs.elk.zone",
+    },
+
+    llms: {
+        domain: "https://docs.elk.zone",
+    },
+
+    vite: {
+        optimizeDeps: {
+            include: ["scule"],
+        },
+    },
+
+    compatibilityDate: "2024-11-07",
+});
